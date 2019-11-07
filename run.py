584c9e8dcec07d1250289fb5c44158fbaa9a2878
Ubuntu <ubuntu@ip-172-31-80-251.ec2.internal>
2019-11-07 15:46:02 +0000

    cleaned up code

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import os
 from datetime import datetime
 from flask import Flask, redirect, render_template, request, session, url_for
 
+
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET", "randomstring123")
 messages = []
@@ -11,14 +12,7 @@ def add_message(username, message):
     """Add messages to the `messages` list"""
     now = datetime.now().strftime("%H:%M:%S")
     messages.append({"timestamp": now, "from": username, "message": message})
-    
-    """ messages.append("({}) {}: {}".format(now, username, message)) """
- 
-"""
-def get_all_messages():
-    # Get all of the messages and separate them with a `br`#
-    return "<br>".join(messages)    
-"""
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -26,37 +20,24 @@ def index():
     if request.method == "POST":
         session["username"] = request.form["username"]
 
-    if "username" in session: 
+    if "username" in session:
         return redirect(url_for("user", username=session["username"]))
-        
-    """ to check if username exists in session and if so we redirect to contents of username session, which takes us to the .route("/<username>") underneath """
-   
+
     return render_template("index.html")
 
 
 @app.route("/chat/<username>", methods=["GET", "POST"])
 def user(username):
-    """Add and display chat messages using post method"""
+    """Add and display chat messages"""
     if request.method == "POST":
         username = session["username"]
         message = request.form["message"]
         add_message(username, message)
         return redirect(url_for("user", username=session["username"]))
-        # redirect unless code reload and sends each 5 seconds
-    
-    return render_template("chat.html", username=username,
-                           chat_messages=messages) 
-                           
-    """ return "<h1>Welcome, {0}</h1>{1}".format(username, messages) """
-    """ removed get all messages replaced with messages list """
 
-""" 
-@app.route("/<username>/<message>")
-def send_message(username, message):
-    #Create a new message and redirect back to the chat page
-    add_messages(username, message)
-    return redirect("/" + username)
-""" 
+    return render_template("chat.html", username=username,
+                           chat_messages=messages)
+
 
 app.run(host=os.getenv("IP", "0.0.0.0"),
         port=int(os.getenv("PORT", "5000")), debug=False)
